5dbc56c9a57126e827a35f6b194ba87fd5f898ac
Niroshan112 <121544895+Niroshan112@users.noreply.github.com>
2025-02-18 16:13:27 +0530

    CRAWL 🚀

--- config-v2.js
+++ config-v2.js
@@ -1,4 +1,4 @@
-const GITHUB_AUTH_TOKEN = "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"
+const GITHUB_AUTH_TOKEN = "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"
 const BOT_NUMBER = "94716682532"
 const SESSION_ID = "PRABATH-MD~cdQmFTbC#mEeftxPLjiz6rln7dEMm_XPoYbKloTL0p-ecp2zflEc"
 
